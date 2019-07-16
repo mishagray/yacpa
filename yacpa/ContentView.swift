140f5337188d16febe2f8d49276b39646334aa4d
Michael Gray <m@voxability.com>
2019-07-16 13:57:15 -0400

    Initial Commit

---
+++ yacpa/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  yacpa
+//
+//  Created by Michael Gray on 7/16/19.
+//  Copyright © 2019 Michael Gray. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
